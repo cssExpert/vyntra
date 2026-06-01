b78762f9c2e003e4db8ba9257c7341facae72169
Simranjeet Singh <simranjeet1012@gmail.com>
2026-06-01 13:36:05 +0530

    Remove glob-parent package and associated files; delete unused types and interfaces from libs/types

--- libs/types/dist/common.types.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-// Generic API envelope + pagination contracts.
-Object.defineProperty(exports, "__esModule", { value: true });
--- libs/types/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './auth.types';
-export * from './package.types';
-export * from './org.types';
-export * from './common.types';
